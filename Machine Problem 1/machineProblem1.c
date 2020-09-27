683271f8a2cc35c28373c560fbc4f578b5728d65
Tasha Ogoti <tsofyx@mail.missouri.edu>
2020-09-26 18:27:44 -0700

    Update machineProblem1.c

--- Machine Problem 1/machineProblem1.c
+++ Machine Problem 1/machineProblem1.c
@@ -1,9 +1,18 @@
-initList()
+struct node{
+	int data;
+	struct node *next;
+};
+
+struct adjList{
+	struct node *graph;
+};
+
+struct adjList* initList(void)
 {
   return * headNode;
 }
 
-createNode(int a, int b)
+createNode(int a)
 {
   
 }

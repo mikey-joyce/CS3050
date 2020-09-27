88194aafde6e808888f69f3dece4f96bb4f8bbe5
sarahduong <55358698+sarahduong@users.noreply.github.com>
2020-09-26 20:33:21 -0500

    Update machineProblem1.c

--- Machine Problem 1/machineProblem1.c
+++ Machine Problem 1/machineProblem1.c
@@ -1,18 +1,41 @@
-struct node{
+struct node
+{
 	int data;
 	struct node *next;
 };
 
-struct adjList{
+struct adjList
+{
 	struct node *graph;
 };
 
 struct adjList* initList(void)
 {
-  return * headNode;
+	return * headNode;
 }
 
-createNode(int a)
+readFile()
 {
-  
+	return fPtr;
 }
+
+createNode(Node * headNode, int a, int b)
+{
+	return newNode;
+}
+
+addNode()
+{
+     createNode	
+}
+
+compare(Node * prevNode, Node * curNode)
+{
+	// compare node for inc size	
+}
+
+printList(Node * headNode)
+{
+	return void;
+}
+

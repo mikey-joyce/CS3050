60e24d317683dd84dbb76631932e73f5a1903eb7
sarahduong <55358698+sarahduong@users.noreply.github.com>
2020-10-15 23:24:03 -0500

    Update machineProblem2.c

--- Machine Problem 2/machineProblem2.c
+++ Machine Problem 2/machineProblem2.c
@@ -4,405 +4,379 @@ Sarah Duong
 Tasha Ogoti
 */
 
-#include<stdlib.h>
-#include<stdio.h>
-#include<stdbool.h>
-#include<string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
 
 //Represents an adjacency list node
-typedef struct adjlistnodestruct{
-  int destination;
-  struct adjlistnodestruct *next;
-}AdjListNode;
+typedef struct adjlistnodestruct
+{
+    int destination;
+    struct adjlistnodestruct *next;
+} AdjListNode;
 
 //Represents and adjacency list
-typedef struct adjliststruct{
-  AdjListNode *head;
-}AdjList;
-
-/*Basic Linked list
-Used as a list that each node contains a separate adj list*/
-typedef struct liststruct{
-  int indegree; //the idea of this is to keep track of indegree
-  int outdegree; //the idea of this is to keep track of outdegree
-	AdjList *myAdjList;
-	struct liststruct *next;
-}List;
+typedef struct adjliststruct
+{
+    AdjListNode *head;
+} AdjList;
 
 //Represents a graph
-typedef struct graphstruct{
-  int totalVertices;
-  int *visited;
-  List *myList;
-}Graph;
+typedef struct graphstruct
+{
+    int totalVertices;
+    int *visited;
+    AdjList *myArray;
+} Graph;
 
 //Graph prototypes
 AdjListNode *createNode(int);
 Graph *createGraph(int);
 int readVertices(FILE *);
 void readEdges(FILE *, Graph *, Graph *);
-void addEdge(Graph *,Graph *, int, int);
+void addEdge(Graph *, Graph *, int, int);
 void printOutdegrees(Graph *);
-void fillStack(Graph *, int, bool *, int *, int);
-void DFS(Graph *, int, bool *);
+int fillStack(Graph *, int, bool *, int *, int);
+void DFS(Graph *, int, bool *, int *, int);
 void transposeGraph(Graph *, int, int);
-List *findTargetNode(Graph *, int);
 void findSCC(Graph *, Graph *, int, int *, int);
 
 //Stack Function Prototypes
 int push(int *, int, int);
 int pop(int);
 
-int main(void){
-  //open file
-	FILE *fPtr = fopen("graph.txt", "r");
+void findOutdegrees(FILE *, Graph *, int *);
 
-  //find total vertices
-  int totalVertices=readVertices(fPtr);
+int main(void)
+{
+    //open file
+    FILE *fPtr = fopen("graph.txt", "r");
 
-  //set pointer back to beginning of file
-  rewind(fPtr);
+    //find total vertices
+    int totalVertices = readVertices(fPtr);
 
-  //set the stack up
-  int myStack[totalVertices];
-  int stackCounter=-1;
+    //set pointer back to beginning of file
+    rewind(fPtr);
 
-  //Find graph data
-  printf("Create graph\n");
-  Graph *graphData1=createGraph(totalVertices);
-  Graph *graphData2=createGraph(totalVertices);
+    //set the stack up
+    int myStack[totalVertices];
+    int stackCounter = -1;
 
-  //read in the edges
-  printf("Read edges\n");
-  readEdges(fPtr, graphData1, graphData2);
+    //Find graph data
+    printf("Create graph\n");
+    Graph *graphData1 = createGraph(totalVertices);
+    Graph *graphData2 = createGraph(totalVertices);
 
-  //find the SCC graph
-  printf("Find scc\n");
-  findSCC(graphData1, graphData2, totalVertices, myStack, stackCounter);
+    //read in the edges
+    printf("Read edges\n");
+    readEdges(fPtr, graphData1, graphData2);
 
-  //function for finding outdegrees maybe can be done within SCC
-  //findOutdegrees() or printOutdegrees() (or both?)
-  //findOutdegrees(graphData1);
-  //printOutdegrees(graphData2);
-	//readFile(fPtr);
+    //find the SCC graph
+    printf("Find scc\n");
+    findSCC(graphData1, graphData2, totalVertices, myStack, stackCounter);
+    printf("SCC found\n");
+    //function for finding outdegrees maybe can be done within SCC
+    //findOutdegrees() or printOutdegrees() (or both?)
+    findOutdegrees(fPtr, graphData1, graphData1->visited);
+    //printOutdegrees(graphData2);
+    //readFile(fPtr);
 
-  //close file and set pointer to NULL
-	fclose(fPtr);
-	fPtr = NULL;
+    //close file and set pointer to NULL
+    fclose(fPtr);
+    fPtr = NULL;
 
-  return 0;
+    return 0;
 }
 
-void readEdges(FILE *fPtr, Graph *graphData1, Graph *graphData2){
-  //while we are not at the end of the file
-  while(!feof(fPtr))
-	{
-    //scan in the edge and add it into our graph
-		int v1, v2;
-		fscanf(fPtr, "%d %d\n", &v1, &v2);
-		addEdge(graphData1, graphData2, v1, v2);
-	}
+void readEdges(FILE *fPtr, Graph *graphData1, Graph *graphData2)
+{
+    //while we are not at the end of the file
+    while (!feof(fPtr))
+    {
+        //scan in the edge and add it into our graph
+        int v1, v2;
+        fscanf(fPtr, "%d %d\n", &v1, &v2);
+        addEdge(graphData1, graphData2, v1, v2);
+    }
 }
 
-int readVertices(FILE *fPtr){
-  //track the maximum possible vertices and total vertices
-  int maxVertices=0;
-  int counter=0;
+int readVertices(FILE *fPtr)
+{
+    //track the maximum possible vertices and total vertices
+    int maxVertices = 0;
+    int counter = 0;
 
-  //find maximum possible vertices
-  while(!feof(fPtr)){
-    int v1, v2;
-    fscanf(fPtr, "%d %d\n", &v1, &v2);
-    maxVertices+=2;
-  }
-  printf("%d\n", maxVertices);
-
-  rewind(fPtr);
-
-  //create an array of unique vertices if its of value -1 then it is not a vertex
-  int uniqueVertices[maxVertices];
-  memset(uniqueVertices, -1, maxVertices*sizeof(int));
-
-  //while we are not at end of file
-  while(!feof(fPtr)){
-    //temp vertices and our totalVertices counter
-    int vertexA, vertexB;
-
-    //scan in vertices
-    fscanf(fPtr, "%d %d\n", &vertexA, &vertexB);
-
-    //loop through entire array
-    for(int i=0; i<maxVertices; i++){
-      //if vertexA isn't unique then break
-      if(vertexA==uniqueVertices[i]){
-        break;
-      }
-      //else if i has reached the end of the array
-      else if(i==(maxVertices-1)){
-        //add vertexA into the array at point counter
-        uniqueVertices[counter]=vertexA;
-        //increment counter and break
-        counter++;
-        break;
-      }
+    //find maximum possible vertices
+    while (!feof(fPtr))
+    {
+        int v1, v2;
+        fscanf(fPtr, "%d %d\n", &v1, &v2);
+        maxVertices += 2;
     }
-    //loop through entire array
-    for(int j=0; j<maxVertices; j++){
-      //if vertexB isn't unique then break
-      if(vertexB==uniqueVertices[j]){
-        break;
-      }
-      //else if j has recahed the end of the array
-      else if(j==(maxVertices-1)){
-        //add vertexB into the array at point counter
-        uniqueVertices[counter]=vertexB;
-        //increment counter and break
-        counter++;
-        break;
-      }
-    }
-  }
 
-  return counter;
+    rewind(fPtr);
+
+    //create an array of unique vertices if its of value -1 then it is not a vertex
+    int uniqueVertices[maxVertices];
+    memset(uniqueVertices, -1, maxVertices * sizeof(int));
+
+    //while we are not at end of file
+    while (!feof(fPtr))
+    {
+        //temp vertices and our totalVertices counter
+        int vertexA, vertexB;
+
+        //scan in vertices
+        fscanf(fPtr, "%d %d\n", &vertexA, &vertexB);
+
+        //loop through entire array
+        for (int i = 0; i < maxVertices; i++)
+        {
+            //if vertexA isn't unique then break
+            if (vertexA == uniqueVertices[i])
+            {
+                break;
+            }
+            //else if i has reached the end of the array
+            else if (i == (maxVertices - 1))
+            {
+                //add vertexA into the array at point counter
+                uniqueVertices[counter] = vertexA;
+                //increment counter and break
+                counter++;
+                break;
+            }
+        }
+        //loop through entire array
+        for (int j = 0; j < maxVertices; j++)
+        {
+            //if vertexB isn't unique then break
+            if (vertexB == uniqueVertices[j])
+            {
+                break;
+            }
+            //else if j has recahed the end of the array
+            else if (j == (maxVertices - 1))
+            {
+                //add vertexB into the array at point counter
+                uniqueVertices[counter] = vertexB;
+                //increment counter and break
+                counter++;
+                break;
+            }
+        }
+    }
+
+    return counter;
 }
 
-List *findTargetNode(Graph *graph, int target){
-  //Initialize our current node and create counter
-  List *currentNode;
-  currentNode = graph->myList;
-  int count=0;
+void findSCC(Graph *graphData1, Graph *graphData2, int vertices, int *myStack, int stackCounter)
+{
+    bool visited[vertices];
+    int count = 1;
 
-  //traverse the list until we hit target node
-  while(count!=target){
-    //increment to next until target node is found
-    currentNode=currentNode->next;
-    count++;
-  }
-
-  //return target node
-  return currentNode;
-}
-
-
-void findSCC(Graph *graphData1, Graph *graphData2, int vertices, int *myStack, int stackCounter){
-  bool visited[vertices];
-  int count=1;
-
-  for(int i=0; i<vertices; i++){
-    visited[i]=false;
-  }
-
-  for(int j=0; j<vertices; j++){
-    if(visited[j]==false){
-      fillStack(graphData1, j, visited, myStack, stackCounter);
+    for (int i = 0; i < vertices; i++)
+    {
+        visited[i] = false;
     }
-  }
 
-  for(int k=0; k<vertices; k++){
-    visited[k]=false;
-  }
-
-  while(stackCounter!=-1){
-    int targetIndex=myStack[stackCounter];
-    stackCounter=pop(stackCounter);
-
-    if(visited[targetIndex]==false){
-      printf("SCC %d: \n", count++);
-      DFS(graphData2, vertices, visited);
-      printf("\n");
+    for (int j = 0; j < vertices; j++)
+    {
+        if (visited[j] == false)
+        {
+            stackCounter = fillStack(graphData1, j, visited, myStack, stackCounter);
+        }
     }
-  }
+
+    for (int k = 0; k < vertices; k++)
+    {
+        visited[k] = false;
+    }
+
+    int *indexes = malloc(sizeof(int) * (vertices + 1));
+    while (stackCounter != -1)
+    {
+        int targetIndex = myStack[stackCounter];
+        stackCounter = pop(stackCounter);
+
+        if (visited[targetIndex] == false)
+        {
+            printf("SCC %d: \n", count++);
+            DFS(graphData2, vertices, visited, indexes, count);
+            printf("\n");
+        }
+    }
 }
 
 //create a new adj list node
-AdjListNode *createNode(int data){
-  //Declare and allocate memory to the new node
-  AdjListNode * newNode = malloc(sizeof(AdjListNode));
+AdjListNode *createNode(int data)
+{
+    //Declare and allocate memory to the new node
+    AdjListNode *newNode = malloc(sizeof(AdjListNode));
 
-  //check to see if malloc worked
-  if(newNode){
-    //initialize our data
-    newNode->destination = data;
-    newNode->next=NULL;
-  }
+    //check to see if malloc worked
+    if (newNode)
+    {
+        //initialize our data
+        newNode->destination = data;
+        newNode->next = NULL;
+    }
 
-  return newNode;
+    return newNode;
 }
 
 //create a graph with v amount of vertices
-Graph* createGraph(int vertices){
-  //declare and allocate memory to the new graph
-  Graph *newGraph=malloc(sizeof(Graph));
+Graph *createGraph(int vertices)
+{
+    //declare and allocate memory to the new graph
+    Graph *newGraph = malloc(sizeof(Graph));
 
-  //if malloc failed
-  if(newGraph == NULL){
-    printf("Memory allocation failed at createGraph (1).\n");
-    return NULL;
-  }
+    //set vertices to amount of vertices given and allocate memory
+    newGraph->totalVertices = vertices;
+    newGraph->myArray = malloc(vertices * sizeof(AdjList));
 
-  printf("Seg fault 1\n");
-  //set vertices to amount of vertices given and allocate memory
-  newGraph->totalVertices = vertices;
-  newGraph->myList=malloc(vertices*sizeof(List));
-
-  printf("Seg fault 2\n");
-  //alias to simplify
-  List *alias = newGraph->myList;
-
-  //if malloc failed
-  if(!alias){
-    printf("Memory allocation failed at createGraph (2).\n");
-    return NULL;
-  }
-
-  printf("Seg fault 3\n");
-  //initialize our data
-  alias->indegree = 0;
-  alias->outdegree = 0;
-  printf("Seg fault 3.1\n");
-  //alias->myAdjList->head=NULL;
-  alias->myAdjList = NULL;
-  printf("Seg fault 3.2\n");
-
-  printf("Seg fault 4\n");
-  //traverse the list
-  for(int i=0; i<=vertices; i++){
-    List *newListNode=malloc(sizeof(List));
-
-    //if malloc failed
-    if(!newListNode){
-      printf("Memory allocation failed at createGraph (3).\n");
-      return NULL;
-    }
-
-    printf("Seg fault 5\n");
-    //initialize our data and increment to next
-    newListNode->indegree = 0;
-    newListNode->outdegree = 0;
-    //newListNode->myAdjList=NULL;
-    newListNode->myAdjList = NULL;
-    alias->next = newListNode;
-    alias = newListNode;
-    printf("Seg fault 6\n");
-  }
-
-  return newGraph;
-}
-
-void DFS(Graph *myGraph, int num, bool visited[]){
-  //find target node
-  List *targetNode = findTargetNode(myGraph, num);
-
-  //keep track of visited
-  visited[num] = true;
-
-  //print dfs
-  printf("%d ", num);
-
-  //set current head to be head of the targetNodes list
-  AdjListNode *currentHead = targetNode->myAdjList->head;
-
-  //while currentHead exists
-  while(currentHead!=NULL)
-  {
-    //if the current head destination doesn't exist in visited
-    if (!visited[currentHead->destination])
+    //traverse the list
+    for (int i = 0; i <= vertices; i++)
     {
-      //call DFS
-      DFS(myGraph, currentHead->destination, visited);
+        //initialize this to NULL
+        newGraph->myArray[i].head = NULL;
     }
-    //increment hold to next
-    currentHead = currentHead->next;
-  }
+
+    return newGraph;
 }
 
-void addEdge(Graph *graphData1, Graph *graphData2, int source, int destination){
-  //create a new node
-  AdjListNode *newNode = createNode(destination);
+void DFS(Graph *myGraph, int num, bool visited[], int *indexes, int count)
+{
+    indexes[num] = count;
+    //keep track of visited
+    visited[num] = true;
 
-  //find target nodes for both lists
-  List *myList1=findTargetNode(graphData1, source);
-  List *myList2=findTargetNode(graphData2, source);
+    //print dfs
+    printf("%d ", num);
 
-  //make next to the new node to be the head of list one
-  newNode->next = myList1->myAdjList->head;
+    //set current head to be head of the targetNodes list
+    AdjListNode *currentHead = myGraph->myArray[num].head;
 
-  //make the head of list two be the new node
-  myList2->myAdjList->head = newNode;
-
-  //transpose our graph
-  transposeGraph(graphData2, source, destination);
-}
-
-void fillStack(Graph *graphData, int data, bool visited[], int *myStack, int stackCounter){
-  //find target node
-  List *targetNode = findTargetNode(graphData, data);
-  //array of visited
-  visited[data] = true;
-
-  //set n to be head of the list that belongs to the target node
-  AdjListNode *currentNode = targetNode->myAdjList->head;
-
-  //while our node exists
-  while(currentNode)
-  {
-    //if our nodes destination doesn't exist in visited
-    if(!visited[currentNode->destination])
+    //while currentHead exists
+    while (currentHead)
     {
-      //fill the stack
-      fillStack(graphData, currentNode->destination, visited, myStack, stackCounter);
+        //if the current head destination doesn't exist in visited
+        if (!visited[currentHead->destination])
+        {
+            //call DFS
+            DFS(myGraph, currentHead->destination, visited, indexes, count);
+        }
+        //increment hold to next
+        currentHead = currentHead->next;
     }
-    //increment current node to next
-    currentNode = currentNode->next;
-  }
-  stackCounter=push(myStack, stackCounter, data);
 }
 
-void transposeGraph(Graph * graphData, int source, int destination){
-  //create new node
-  AdjListNode *newNode = createNode(source);
+void addEdge(Graph *graphData1, Graph *graphData2, int source, int destination)
+{
+    //create a new node
+    AdjListNode *newNode = createNode(destination);
 
-  //Find target node
-  List *list=findTargetNode(graphData, destination);
+    //make next to the new node to be the head of list one
+    newNode->next = graphData1->myArray[source].head;
 
-  //set next of new node to be current head
-  newNode->next = list->myAdjList->head;
+    //make the head of list two be the new node
+    graphData2->myArray[source].head = newNode;
 
-  //set the current head to be the new node
-  list->myAdjList->head = newNode;
+    //transpose our graph
+    transposeGraph(graphData2, source, destination);
 }
 
-void findOutdegrees(){
+int fillStack(Graph *graphData, int data, bool visited[], int *myStack, int stackCounter)
+{
+    //array of visited
+    visited[data] = true;
 
+    //set n to be head of the list that belongs to the target node
+    AdjListNode *currentNode = graphData->myArray[data].head;
+
+    //while our node exists
+    while (currentNode)
+    {
+        //if our nodes destination doesn't exist in visited
+        if (!visited[currentNode->destination])
+        {
+            //fill the stack
+            fillStack(graphData, currentNode->destination, visited, myStack, stackCounter);
+        }
+        //increment current node to next
+        currentNode = currentNode->next;
+    }
+    stackCounter = push(myStack, stackCounter, data);
+
+    return stackCounter;
 }
 
-//Changed this function to print out degree since we dont need to print graph
-void printOutdegrees(Graph *mySCC){
-  printf("The outdegrees are: ");
-  int count=0;
-  int limit = mySCC->myList->outdegree;
-  while(count!=limit)
-  {
-    int data = mySCC->myList->myAdjList->head->destination;
-    count++;
-    printf("%d ",data);
-  }
+void transposeGraph(Graph *graphData, int source, int destination)
+{
+    //create new node
+    AdjListNode *newNode = createNode(source);
+
+    //set next of new node to be current head
+    newNode->next = graphData->myArray[destination].head;
+
+    //set the current head to be the new node
+    graphData->myArray[destination].head = newNode;
+}
+
+void linkSCCs(int source, int SCCfound, Graph *outDegreeGraph)
+{
+    AdjListNode *hold = outDegreeGraph->myArray[source].head;
+
+    while (hold)
+    {
+        // if connection previously found, keep going
+        if (hold->destination == SCCfound)
+        {
+            return;
+        }
+        else
+        {
+            // keep moving if not found
+            hold = hold->next;
+        }
+    }
+
+    AdjListNode *myNewNode = createNode(SCCfound);
+
+    myNewNode->next = outDegreeGraph->myArray[source].head;
+    outDegreeGraph->myArray[source].head = myNewNode;
+}
+
+void findOutdegrees(FILE *fPtr, Graph *outDeg, int index[])
+{
+    int v1, v2;
+
+    while (fscanf(fPtr, "%d %d", &v1, &v2))
+    {
+        if (index[v1] != index[v2])
+        {
+            linkSCCs(index[v1], index[v2], outDeg);
+        }
+    }
 }
 
 //Stack functions
-int push(int *myStack, int stackCounter, int targetIndex){
-  //increment to the first index
-  stackCounter++;
+int push(int *myStack, int stackCounter, int targetIndex)
+{
+    //increment to the first index
+    stackCounter++;
 
-  //store the target index in the stack
-  myStack[stackCounter]=targetIndex;
+    //store the target index in the stack
+    myStack[stackCounter] = targetIndex;
 
-  return stackCounter;
+    return stackCounter;
 }
 
-int pop(int stackCounter){
-  //decrement our stack
-  stackCounter--;
+int pop(int stackCounter)
+{
+    //decrement our stack
+    stackCounter--;
 
-  return stackCounter;
+    return stackCounter;
 }

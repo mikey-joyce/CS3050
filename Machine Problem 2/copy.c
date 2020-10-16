a09b4848d0d0fc34f126bf4758908ea36c7c9b4b
Mikey Joyce <joyce.mp2019@gmail.com>
2020-10-15 19:14:30 -0500

    Added Copy
    Added the copy file.

--- Machine Problem 2/copy.c
+++ Machine Problem 2/copy.c
@@ -20,20 +20,11 @@ typedef struct adjliststruct{
   AdjListNode *head;
 }AdjList;
 
-/*Basic Linked list
-Used as a list that each node contains a separate adj list*/
-typedef struct liststruct{
-  int indegree; //the idea of this is to keep track of indegree
-  int outdegree; //the idea of this is to keep track of outdegree
-	AdjList *myAdjList;
-	struct liststruct *next;
-}List;
-
 //Represents a graph
 typedef struct graphstruct{
   int totalVertices;
   int *visited;
-  List *myList;
+  AdjList *myArray;
 }Graph;
 
 //Graph prototypes
@@ -46,7 +37,6 @@ void printOutdegrees(Graph *);
 void fillStack(Graph *, int, bool *, int *, int);
 void DFS(Graph *, int, bool *);
 void transposeGraph(Graph *, int, int);
-List *findTargetNode(Graph *, int);
 void findSCC(Graph *, Graph *, int, int *, int);
 
 //Stack Function Prototypes
@@ -79,7 +69,7 @@ int main(void){
   //find the SCC graph
   printf("Find scc\n");
   findSCC(graphData1, graphData2, totalVertices, myStack, stackCounter);
-
+  printf("SCC found\n");
   //function for finding outdegrees maybe can be done within SCC
   //findOutdegrees() or printOutdegrees() (or both?)
   //findOutdegrees(graphData1);
@@ -115,7 +105,6 @@ int readVertices(FILE *fPtr){
     fscanf(fPtr, "%d %d\n", &v1, &v2);
     maxVertices+=2;
   }
-  printf("%d\n", maxVertices);
 
   rewind(fPtr);
 
@@ -166,24 +155,6 @@ int readVertices(FILE *fPtr){
   return counter;
 }
 
-List *findTargetNode(Graph *graph, int target){
-  //Initialize our current node and create counter
-  List *currentNode;
-  currentNode = graph->myList;
-  int count=0;
-
-  //traverse the list until we hit target node
-  while(count!=target){
-    //increment to next until target node is found
-    currentNode=currentNode->next;
-    count++;
-  }
-
-  //return target node
-  return currentNode;
-}
-
-
 void findSCC(Graph *graphData1, Graph *graphData2, int vertices, int *myStack, int stackCounter){
   bool visited[vertices];
   int count=1;
@@ -201,7 +172,6 @@ void findSCC(Graph *graphData1, Graph *graphData2, int vertices, int *myStack, i
   for(int k=0; k<vertices; k++){
     visited[k]=false;
   }
-
   while(stackCounter!=-1){
     int targetIndex=myStack[stackCounter];
     stackCounter=pop(stackCounter);
@@ -240,59 +210,26 @@ Graph* createGraph(int vertices){
     return NULL;
   }
 
-  printf("Seg fault 1\n");
   //set vertices to amount of vertices given and allocate memory
   newGraph->totalVertices = vertices;
-  newGraph->myList=malloc(vertices*sizeof(List));
-
-  printf("Seg fault 2\n");
-  //alias to simplify
-  List *alias = newGraph->myList;
+  newGraph->myArray=malloc(vertices*sizeof(AdjList));
 
   //if malloc failed
-  if(!alias){
+  if(!newGraph->myArray){
     printf("Memory allocation failed at createGraph (2).\n");
     return NULL;
   }
 
-  printf("Seg fault 3\n");
-  //initialize our data
-  alias->indegree = 0;
-  alias->outdegree = 0;
-  printf("Seg fault 3.1\n");
-  //alias->myAdjList->head=NULL;
-  alias->myAdjList = NULL;
-  printf("Seg fault 3.2\n");
-
-  printf("Seg fault 4\n");
   //traverse the list
   for(int i=0; i<=vertices; i++){
-    List *newListNode=malloc(sizeof(List));
-
-    //if malloc failed
-    if(!newListNode){
-      printf("Memory allocation failed at createGraph (3).\n");
-      return NULL;
-    }
-
-    printf("Seg fault 5\n");
-    //initialize our data and increment to next
-    newListNode->indegree = 0;
-    newListNode->outdegree = 0;
-    //newListNode->myAdjList=NULL;
-    newListNode->myAdjList = NULL;
-    alias->next = newListNode;
-    alias = newListNode;
-    printf("Seg fault 6\n");
+    //initialize this to NULL
+    newGraph->myArray[i].head=NULL;
   }
 
   return newGraph;
 }
 
 void DFS(Graph *myGraph, int num, bool visited[]){
-  //find target node
-  List *targetNode = findTargetNode(myGraph, num);
-
   //keep track of visited
   visited[num] = true;
 
@@ -300,10 +237,10 @@ void DFS(Graph *myGraph, int num, bool visited[]){
   printf("%d ", num);
 
   //set current head to be head of the targetNodes list
-  AdjListNode *currentHead = targetNode->myAdjList->head;
+  AdjListNode *currentHead = myGraph->myArray[num].head;
 
   //while currentHead exists
-  while(currentHead!=NULL)
+  while(currentHead)
   {
     //if the current head destination doesn't exist in visited
     if (!visited[currentHead->destination])
@@ -320,28 +257,22 @@ void addEdge(Graph *graphData1, Graph *graphData2, int source, int destination){
   //create a new node
   AdjListNode *newNode = createNode(destination);
 
-  //find target nodes for both lists
-  List *myList1=findTargetNode(graphData1, source);
-  List *myList2=findTargetNode(graphData2, source);
-
   //make next to the new node to be the head of list one
-  newNode->next = myList1->myAdjList->head;
+  newNode->next = graphData1->myArray[source].head;
 
   //make the head of list two be the new node
-  myList2->myAdjList->head = newNode;
+  graphData2->myArray[source].head = newNode;
 
   //transpose our graph
   transposeGraph(graphData2, source, destination);
 }
 
 void fillStack(Graph *graphData, int data, bool visited[], int *myStack, int stackCounter){
-  //find target node
-  List *targetNode = findTargetNode(graphData, data);
   //array of visited
   visited[data] = true;
 
   //set n to be head of the list that belongs to the target node
-  AdjListNode *currentNode = targetNode->myAdjList->head;
+  AdjListNode *currentNode = graphData->myArray[data].head;
 
   //while our node exists
   while(currentNode)
@@ -362,31 +293,15 @@ void transposeGraph(Graph * graphData, int source, int destination){
   //create new node
   AdjListNode *newNode = createNode(source);
 
-  //Find target node
-  List *list=findTargetNode(graphData, destination);
-
   //set next of new node to be current head
-  newNode->next = list->myAdjList->head;
+  newNode->next = graphData->myArray[destination].head;
 
   //set the current head to be the new node
-  list->myAdjList->head = newNode;
+  graphData->myArray[destination].head = newNode;
 }
 
 void findOutdegrees(){
 
-}
-
-//Changed this function to print out degree since we dont need to print graph
-void printOutdegrees(Graph *mySCC){
-  printf("The outdegrees are: ");
-  int count=0;
-  int limit = mySCC->myList->outdegree;
-  while(count!=limit)
-  {
-    int data = mySCC->myList->myAdjList->head->destination;
-    count++;
-    printf("%d ",data);
-  }
 }
 
 //Stack functions
